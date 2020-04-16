fc64b3a70d3cc7915e374852a0b1b132c5db7788
Josh Miller <wickedhillsmedia@gmail.com>
2020-04-16 14:56:48 -0400

    added tape for testing

--- test/main.ts
+++ test/main.ts
@@ -1,1 +1,12 @@
-function test() { }
+import tape from "tape";
+
+tape('timing test', function (t) {
+    t.plan(2);
+
+    t.equal(typeof Date.now, 'function');
+    var start = Date.now();
+
+    setTimeout(function () {
+        t.equal(Date.now() - start, 100);
+    }, 100);
+});
